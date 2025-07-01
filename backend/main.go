864ccfacfe8840b39e7838eef039f3751804d69a
y33550336 <u5yuugoo@gmail.com>
2025-07-01 17:08:50 +0900

    ハンドラーを追加した

--- backend/main.go
+++ backend/main.go
@@ -15,46 +15,66 @@ type Message struct {
 	UserID  string `json:"userId"`
 }
 
+type Handler struct {
+	client *traq.APIClient
+	token  string
+}
+
+func NewHandler(token string, client *traq.APIClient) *Handler {
+	return &Handler{
+		token:  token,
+		client: client,
+	}
+}
+
+func (h *Handler) makeAuth(ctx context.Context) context.Context {
+	return context.WithValue(ctx, traq.ContextAccessToken, h.token)
+}
+
 func main() {
 	token := os.Getenv("TRAQ_TOKEN")
-
 	client := traq.NewAPIClient(traq.NewConfiguration())
-	auth := context.WithValue(context.Background(), traq.ContextAccessToken, token)
+
+	handler := NewHandler(token, client)
 
 	e := echo.New()
-	e.GET("/api/ubugoe/:userId", func(c echo.Context) error {
-		userID := c.Param("userId")
-		fmt.Println("User ID:", userID)
 
-		channelList, _, err := client.ChannelApi.GetChannels(auth).Path("gps/times/" + userID).Execute()
-		//_, _, err := client.UserApi.GetUser(context.Background(), userID).Execute()
-		if err != nil {
-			c.Logger().Error(err)
-			return c.JSON(500, "something went wrong")
-		}
-		if len(channelList.Public) == 0 {
-			return c.JSON(404, "No channels found")
-		}
-		channel := channelList.Public[0]
-		channelID := channel.Id
-
-		messages, _, _ := client.MessageApi.GetMessages(auth, channelID).
-			Since(time.Date(1970, 1, 1, 0, 0, 0, 0, time.Local)).
-			Limit(10).
-			Order("asc").
-			Execute()
-
-		res := make([]Message, 0, len(messages))
-		for _, message := range messages {
-
-			res = append(res, Message{
-				Content: message.Content,
-				UserID:  message.UserId,
-			})
-		}
-
-		return c.JSON(200, res)
-	})
+	e.GET("/api/ubugoe/:userId", handler.GetMessages)
 
 	e.Start(":8080")
+}
+
+func (h *Handler) GetMessages(c echo.Context) error {
+	auth := h.makeAuth(c.Request().Context())
+	userID := c.Param("userId")
+	fmt.Println("User ID:", userID)
+
+	channelList, _, err := h.client.ChannelApi.GetChannels(auth).Path("gps/times/" + userID).Execute()
+	//_, _, err := client.UserApi.GetUser(context.Background(), userID).Execute()
+	if err != nil {
+		c.Logger().Error(err)
+		return c.JSON(500, "something went wrong")
+	}
+	if len(channelList.Public) == 0 {
+		return c.JSON(404, "No channels found")
+	}
+	channel := channelList.Public[0]
+	channelID := channel.Id
+
+	messages, _, _ := h.client.MessageApi.GetMessages(auth, channelID).
+		Since(time.Date(1970, 1, 1, 0, 0, 0, 0, time.Local)).
+		Limit(10).
+		Order("asc").
+		Execute()
+
+	res := make([]Message, 0, len(messages))
+	for _, message := range messages {
+
+		res = append(res, Message{
+			Content: message.Content,
+			UserID:  message.UserId,
+		})
+	}
+
+	return c.JSON(200, res)
 }

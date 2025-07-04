9bc6fda920e1e4f7e17b2bbda74ed1e80cb0bc1e
{mina} <{sykkym.117@gmail.com}>
2025-07-04 23:28:57 +0900

    アイコンのコンポーネントを追加した

---
+++ frontend/src/components/UserIcon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const { id, isHover = true } = defineProps<{
+  id: string
+  isHover?: boolean
+}>()
+
+const imageUrl = ref(`https://q.trap.jp/api/v3/public/icon/${id}`)
+</script>
+
+<template>
+  <div>
+    <img :src="imageUrl" class="usericon" />
+    <v-tooltip v-if="isHover" activator="parent" location="bottom">{{ id }}</v-tooltip>
+  </div>
+</template>
+
+<style scoped>
+.usericon {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.05);
+}
+</style>

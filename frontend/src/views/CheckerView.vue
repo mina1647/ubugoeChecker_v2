fe4f4a824217419c545cf2d6f85c7b163e79664d
{mina} <{sykkym.117@gmail.com}>
2025-07-05 01:49:18 +0900

    使われていない部分をコメントアウト

--- frontend/src/views/CheckerView.vue
+++ frontend/src/views/CheckerView.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import UserMessage from '@/components/Message.vue'
+// import UserMessage from '@/components/Message.vue'
 
 const messages = ref<{ id: string; message: string }[]>([])
 const query = ref<string>('')
